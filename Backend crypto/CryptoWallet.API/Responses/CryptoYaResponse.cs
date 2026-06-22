a06d8b5adec37158c3881ce78252f7c967b95616
Martin <martingiorda4@gmail.com>
2026-06-21 22:29:09 -0300

    Remove comments from CryptoYaResponse class

--- Backend crypto/CryptoWallet.API/Responses/CryptoYaResponse.cs
+++ Backend crypto/CryptoWallet.API/Responses/CryptoYaResponse.cs
@@ -3,17 +3,16 @@ using System.Text.Json.Serialization;
 
 namespace CryptoWallet.API.Responses
 {
-    // ESTA CLASE SIRVE PARA DESERIALIZAR LA RESPUESTA DE LA API DE CRIPTOYA CUANDO SE CONSULTA EL PRECIO DE UNA CRIPTOMONEDA
-    //ESTO ES LO QUE DEVUELVE LA API DE CRIPTOYA CUANDO SE CONSULTA EL PRECIO DE UNA CRIPTOMONEDA
+  
     public class CryptoYaResponse
     {
-        [JsonPropertyName("ask")] // El precio de compra sin comisiones
+        [JsonPropertyName("ask")] 
         public double Ask { get; set; }
-        [JsonPropertyName("totalAsk")] // El precio de compra CON comisiones (lo que realmente pagas al comprar)
+        [JsonPropertyName("totalAsk")] 
         public double TotalAsk { get; set; }
-        [JsonPropertyName("bid")] // El precio de venta sin comisiones
+        [JsonPropertyName("bid")] 
         public double Bid { get; set; }
-        [JsonPropertyName("totalBid")] // El precio de venta CON comisiones (lo que realmente recibis al vender)
+        [JsonPropertyName("totalBid")] 
         public double TotalBid { get; set; }
 
     }
